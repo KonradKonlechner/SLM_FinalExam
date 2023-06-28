905b8d91f4abd49b9d6920c346938a700a7988ac
KonradKonlechner <wi21b012@technikum-wien.at>
2023-06-28 19:01:46 +0200

    adds feature for calculating the factorial of an integer

--- final_exam/src/main/java/fhtw/final_exam/controller/DemoController.java
+++ final_exam/src/main/java/fhtw/final_exam/controller/DemoController.java
@@ -1,24 +1,24 @@
-package fhtw.final_exam.controller;
-
-import fhtw.final_exam.service.DemoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalTime;
-
-@RestController
-@RequestMapping("/api")
-public class DemoController {
-
-        private final DemoService demoService;
-
-        public DemoController(DemoService demoService) {
-            this.demoService = demoService;
-        }
-        @GetMapping("/time")
-        public LocalTime getTime() {
-            return demoService.getTime();
-        }
-
-}
+//package fhtw.final_exam.controller;
+//
+//import fhtw.final_exam.service.DemoService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.time.LocalTime;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class DemoController {
+//
+//        private final DemoService demoService;
+//
+//        public DemoController(DemoService demoService) {
+//            this.demoService = demoService;
+//        }
+//        @GetMapping("/time")
+//        public LocalTime getTime() {
+//            return demoService.getTime();
+//        }
+//
+//}
